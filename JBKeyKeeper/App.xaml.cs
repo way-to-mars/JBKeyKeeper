379783c12d878b326aa219a2bc93120348d3dbaf
job-pc <way2mars@hotmail.com>
2023-10-17 15:06:23 +0700

    Добавьте файлы проекта.

---
+++ JBKeyKeeper/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Text.Json;
+using System;
+
+namespace JBKeyKeeper
+{
+    public partial class App : Application
+    {
+        public App() { Startup += new StartupEventHandler(OnAppStart); }
+
+        void OnAppStart(object sender, StartupEventArgs e) {
+
+            JBKKContainer jbbk = new JBKKContainer
+            {
+                Name = "Thomas",
+                Format = "0",
+                History = new List<JBBKItem> { 
+                    new JBBKItem{ 
+                        Date = "02.02.2023",
+                        Name = "Bank account",
+                        Fields = new List<JBBKPair> {
+                            new JBBKPair{ Name = "login", Value = "Thomas"},
+                            new JBBKPair{ Name = "password", Value = "1234567890"},
+                            new JBBKPair{ Name = "Description", Value = "Don't tell anyone"},
+                        }
+                    },
+                    new JBBKItem{
+                        Date = "12.09.2023",
+                        Name = "Bank account",
+                        Fields = new List<JBBKPair> {
+                            new JBBKPair{ Name = "login", Value = "Thomas"},
+                            new JBBKPair{ Name = "password", Value = "01231230"},
+                            new JBBKPair{ Name = "Description", Value = "new password"},
+                        }
+                    }
+                }
+            };
+
+            var jbbkSerialized = JsonSerializer.Serialize(jbbk);
+
+            Console.WriteLine("\n" + jbbkSerialized.ToString() + "\n");   
+
+            this.Shutdown();
+        }
+    }
+}
---
+++ JBKeyKeeper/Json/JBKKContainer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+
+namespace JBKeyKeeper
+{
+    internal class JBKKContainer
+    {
+        public const string TAG = "JsonBasedKeyKeeper";
+        public string Format { get; set; }
+        public string Name { get; set; }
+        public List<JBBKItem> History { get; set; }
+    }
+
+    internal class JBBKItem
+    {
+        public string Date { get; set; }
+        public string Name { get; set; }
+        public List<JBBKPair> Fields { get; set; }
+    }
+
+    internal class JBBKPair { 
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
